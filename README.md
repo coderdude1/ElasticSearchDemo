e8a28b100dcfae2e456ef72a2dfc0afd1e7708aa
Randy <drbugsmn@gmail.com>
2016-08-30 09:14:53 -0500

    Added swagger, basic controllers, readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+This is a simple sandbox to experiment with elasticsearch, springboot support of elastic, and other things 
+related to elastic.
--- src/main/java/com/dood/elastic/ElasticSearchDemoApplication.java
+++ src/main/java/com/dood/elastic/ElasticSearchDemoApplication.java
@@ -2,11 +2,13 @@ package com.dood.elastic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class ElasticSearchDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ElasticSearchDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchDemoApplication.class, args);
+    }
 }
